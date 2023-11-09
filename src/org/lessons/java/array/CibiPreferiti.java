f9789874a0287a9a7395e9494a344f57ac9f4041
niccoloruberti <nikiruberti@gmail.com>
2023-11-09 18:22:56 +0100

    esercizio completato

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -1,5 +1,19 @@
 package org.lessons.java.array;
 
 public class CibiPreferiti {
-
+	
+	public static void main(String[] args) {
+		
+		//creo l'array con la classifica dei miei cibi preferiti
+		String[] favouriteFoods = {"Pizza", "Carbonara", "Bistecca", "Polpette", "Gelato"};
+		
+		//stampo a video la lunghezza della classifica
+		System.out.println("I cibi preferiti sono " + favouriteFoods.length);
+		
+		//stampo il primo cibo in classifica
+		System.out.println("Il mio cibo preferito è: " + favouriteFoods[0]);
+		
+		//stampo l'ultimo cibo preferito
+		System.out.println("L'ultimo cibo preferito è: " + favouriteFoods[favouriteFoods.length - 1]);
+	}
 }
